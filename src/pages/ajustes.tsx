98b2dead09daba258cd2e8185cdb9294e6cfb5cf
Jonata Ramalho <jonata.ramalho@t10.digital>
2022-09-04 19:58:37 -0300

    feat: Cria pagina notificacoes e ajustes

---
+++ src/pages/ajustes.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/template/Layout";
+
+export default function AJustes() {
+  return (
+    <Layout titulo="Ajustes e Configurações"
+      subtitulo="Personalize o sistema por aqui!">
+      <h3>Conteudo</h3>
+    </Layout>
+  )
+}
