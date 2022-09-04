82a906199f764d632749d7ab29d7d662b32cae81
Jonata Ramalho <jonata.ramalho@t10.digital>
2022-09-04 14:13:10 -0300

    feat: Adiciona icones do menu

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,7 +1,14 @@
+import { IconceSino, IconeAjustes, IconeCasa } from "../icons/index";
+import MenuItem from "./MenuItem";
+
 export default function MenuLateral() {
   return (
     <aside>
-      <h1>Menu</h1>
+      <ul>
+        <MenuItem url="/" texto="Início" icone={IconeCasa} />
+        <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+        <MenuItem url="/notificacoes" texto="Notificações" icone={IconceSino} />
+      </ul>
     </aside>
   )
 }
