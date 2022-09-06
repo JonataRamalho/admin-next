30cd17f41fc449665ecab7edfaca9e4717355a97
Jonata Ramalho <jonata.ramalho@t10.digital>
2022-09-05 21:54:36 -0300

    feat: Adiciona o dark

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,10 +1,13 @@
-import { IconceSino, IconeAjustes, IconeCasa } from "../icons/index";
+import { IconceSino, IconeAjustes, IconeCasa, IconeSair } from "../icons/index";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
   return (
-    <aside>
+    <aside className={`
+      flex flex-col
+      dark:bg-gray-900
+    `}>
       <div className={`
         flex flex-col items-center justify-center
         bg-gradient-to-r from-indigo-500 to-purple-800
@@ -12,11 +15,26 @@ export default function MenuLateral() {
       `}>
         <Logo />
       </div>
-      <ul>
+      <ul className="flex-grow">
         <MenuItem url="/" texto="Início" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
         <MenuItem url="/notificacoes" texto="Notificações" icone={IconceSino} />
       </ul>
+      <ul>
+        <MenuItem
+          onClick={() => console.log('logout')}
+          texto="Sair"
+          icone={IconeSair}
+          className={`
+          text-red-600 dark:text-red-400
+          hover:bg-red-400
+          hover:text-white
+          dark:hover:text-white
+          `}
+        />
+      </ul>
     </aside>
+
+
   )
 }
