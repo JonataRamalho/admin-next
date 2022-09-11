770f00557919dcd4b7574e2a20b2733ee71e61a6
Jonata Ramalho <jonata.ramalho@t10.digital>
2022-09-11 12:34:58 -0300

    feat: Adiciona uma segunda forma de proteger rotas

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -1,4 +1,5 @@
 import useAppData from "../../data/hook/useAppData"
+import forcarAutenticacao from "../../functions/forcarAutenticacao"
 import ForcarAutenticacao from "../auth/ForcarAutenticacao"
 import Cabecalho from "./Cabecalho"
 import Conteudo from "./Conteudo"
@@ -13,20 +14,20 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
   const { tema, alternarTema } = useAppData()
 
-  return (
-    <ForcarAutenticacao>
-      <div className={`${tema} flex h-screen w-screen`}>
-        <MenuLateral />
-        <div className={
-          `flex flex-col w-full p-7
+  return forcarAutenticacao(
+    // <ForcarAutenticacao>
+    <div className={`${tema} flex h-screen w-screen`}>
+      <MenuLateral />
+      <div className={
+        `flex flex-col w-full p-7
         bg-gray-300 dark:bg-gray-800
         `}>
-          <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-          <Conteudo>
-            {props.children}
-          </Conteudo>
-        </div>
+        <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+        <Conteudo>
+          {props.children}
+        </Conteudo>
       </div>
-    </ForcarAutenticacao>
+    </div>
+    // </ForcarAutenticacao>
   )
 }
